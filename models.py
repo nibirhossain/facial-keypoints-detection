ee12f49e902f97c6820f852295bf5c01ded82c4c
Nibir Hossain <nibir.hossain85@gmail.com>
2020-07-01 13:22:21 +0200

    Define CNN model structure
    Define Convolutional Neural Network model to detect the face keypoints. NiamishNet paper has been followed to define the model.
    This paper proposed three different models such as superlite, lite and full versioned model. For this project the superlite version has been chosen.
    Two convulational layers, two linear layers and two dropout layers are applied. The model will generate (138) 68-x,y cordinates for the provided face.

--- models.py
+++ models.py
@@ -1,7 +1,6 @@
 ## TODO: define the convolutional neural network architecture
 
 import torch
-from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 # can use the below import should you choose to initialize the weights of your Net
@@ -17,21 +16,36 @@ class Net(nn.Module):
         ## 1. This network takes in a square (same width and height), grayscale image as input
         ## 2. It ends with a linear layer that represents the keypoints
         ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
-        
-        # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
-        # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 32, 5)
-        
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
         
 
+        # input 1x224x224
+        self.conv1 = nn.Conv2d(1, 32, 5)  #32x221x221
+        self.pool1 = nn.MaxPool2d(4, 4)   #32x55x55
+        self.drop1 = nn.Dropout(0.1)
+       
+        self.conv2 = nn.Conv2d(32, 64, 3) #64x53x53
+        self.pool2 = nn.MaxPool2d(4, 4)   #64x13x13
+        self.drop2 = nn.Dropout(0.2)
+
+        self.lin1 = nn.Linear(64*13*13,500)
+        self.drop3 = nn.Dropout(0.3)
+        self.lin2 = nn.Linear(500,68*2)
+        
         
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
         
+        x = self.drop1(self.pool1(F.relu(self.conv1(x))))
+        x = self.drop2(self.pool2(F.relu(self.conv2(x))))
+        
+        x = x.view(x.size(0), -1) # flatten
+        
+        x = self.drop3(F.relu(self.lin1(x)))
+        x = self.lin2(x)
         
         # a modified x, having gone through all the layers of your model, should be returned
         return x
